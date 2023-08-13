854b9a75341059eb96e99e39355d809683228d99
Silva1910 <122049671+Silva1910@users.noreply.github.com>
2023-08-13 15:04:09 -0300

    retificando Exerc S.O

--- src/SO/Controller/Controller.java
+++ src/SO/Controller/Controller.java
@@ -14,12 +14,12 @@ public class Controller {
 		{
 			double temporInicial = System.nanoTime();
 			for (int i = 0; i < 1000; i++) {
-				vetor1[i] =0;
+			
 			}
 			double tempoFinal = System.nanoTime();
 			double tempoTotal = tempoFinal - temporInicial;
 			tempoTotal = tempoTotal / Math.pow(10, 9);
-			System.out.println("o tempo total do vetor 1 e ==>" + tempoTotal + "s"  );
+			System.out.println("o tempo total do vetor 1 e ==>" + String.format("%.7f", tempoTotal) + "s" );
 		}
 	}
 
@@ -27,12 +27,12 @@ public class Controller {
 		{
 			double temporInicial = System.nanoTime();
 			for (int i = 0; i < 10000; i++) {
-				vetor2[i] =0;
+				
 			}
 			double tempoFinal = System.nanoTime();
 			double tempoTotal = tempoFinal - temporInicial;
 			tempoTotal = tempoTotal / Math.pow(10, 9);
-			System.out.println("o tempo total do vetor 2 e ==>" + tempoTotal + "s"  );
+			System.out.println("o tempo total do vetor 2 e ==>" + String.format("%.7f", tempoTotal) + "s" );
 		}
 
 	}
@@ -40,12 +40,13 @@ public class Controller {
 		{
 			double temporInicial = System.nanoTime();
 			for (int i = 0; i < 100000; i++) {
-				vetor3[i] =0;
+				
+			
 			}
 			double tempoFinal = System.nanoTime();
 			double tempoTotal = tempoFinal - temporInicial;
 			tempoTotal = tempoTotal / Math.pow(10, 9);
-			System.out.println("o tempo total do vetor 3 e ==>" + tempoTotal + "s" );
+			System.out.println("o tempo total do vetor 3 e ==>" + String.format("%.7f", tempoTotal) + "s" );
 		}
 
 	}
--- src/SO/Viewer/Principal.java
+++ src/SO/Viewer/Principal.java
@@ -6,17 +6,27 @@ public class Principal {
 
 	public static void main(String[] args) {
 		Controller op = new Controller();
-		
-		int[] vetor1 =  new int[1000];
+
+		int[] vetor1 = new int[1000];
 		int[] vetor2 = new int[10000];
 		int[] vetor3 = new int[100000];
-		
-	op.vt1(vetor1); 
-	op.vt2(vetor2);
-	op.vt3(vetor3);
-		
-	
-	}
 
+		for (int i = 0; i < 1000; i++) {
+			vetor1[i] = 0;
+		}
+
+		for (int i = 0; i < 10000; i++) {
+			vetor2[i] = 0;
+		}
+
+		for (int i = 0; i < 100000; i++) {
+			vetor3[i] = 0;
+		}
+
+		op.vt1(vetor1);
+		op.vt2(vetor2);
+		op.vt3(vetor3);
+
+	}
 
 }
