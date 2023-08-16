3ca628415cdf1c0ce6b97b885069c8cff178ae01
Silva1910 <122049671+Silva1910@users.noreply.github.com>
2023-08-16 01:07:58 -0300

    exercicios aula 2

--- src/AULA2/Exercicio8/Controller/Controller.java
+++ src/AULA2/Exercicio8/Controller/Controller.java
@@ -3,7 +3,18 @@ package AULA2.Exercicio8.Controller;
 public class Controller {
 
 	public Controller() {
-		// TODO Auto-generated constructor stub
+		super();
 	}
 
-}
+	public String invertePalavra(String palavra) {
+        // Return the array when empty.
+        if(palavra.isEmpty()) {
+            return palavra;
+        } else {
+            // Call recursion removing the first letter.
+            // Concatenate the returned string with the original first letter, reversing the original order. 
+            return invertePalavra(palavra.substring(1)).concat(palavra.substring(0,1));
+        }
+    }
+	
+	}
--- src/AULA2/Exercicio8/view/view.java
+++ src/AULA2/Exercicio8/view/view.java
@@ -1,10 +1,13 @@
 package AULA2.Exercicio8.view;
-
+import AULA2.Exercicio8.Controller.*;
 public class view {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Controller Controller = new Controller();
+        String Palavra = "teste";
 
+        String temp = Controller.invertePalavra(Palavra);
+        System.out.println("a palavra original e ==>  "+ "" + Palavra + "" + "  e ela invertida fica assim ==> " + Controller.invertePalavra(Palavra));
 	}
 
 }
