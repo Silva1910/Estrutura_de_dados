ff15ac3f1f6889c4385a817a016e4dd17a5ff9d6
Silva1910 <122049671+Silva1910@users.noreply.github.com>
2023-09-18 17:12:32 -0300

    exercicio ed

---
+++ src/Aula6/Exercicio5sala/controller/FatPilha.java
@@ -0,0 +1,22 @@
+package Aula6.Exercicio5sala.controller;
+
+import model.PilhaInteiros;
+
+public class FatPilha {
+	public FatPilha() {
+		super();
+	}
+
+	public long FatPilha(int valor) throws Exception {
+		PlhaInteiros p = new PlhaInteiros();
+		while (valor > 1) {
+			p.push(valor);
+			valor = valor - 1;
+		}
+		long fat = valor;
+		while (!p.isEmpty()) {
+			fat = fat * p.pop();
+		}
+		return fat;
+	}
+}
---
+++ src/Aula6/Exercicio5sala/view/main.java
@@ -0,0 +1,22 @@
+package Aula6.Exercicio5sala.view;
+
+import javax.swing.JOptionPane;
+import   Aula6.Exercicio5sala.controller.FatPilha;
+
+public class main {
+
+    public static void main(String[] args) {
+        int num = 0;
+        do {
+            num = Integer.parseInt(JOptionPane.showInputDialog("Digite um número"));
+        } while (num > 10 || num < 0);
+
+        FatPilha fc = new FatPilha();
+
+        try {
+            System.out.println("O fatorial de " + num + " é " + fc.FatPilha(num));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
